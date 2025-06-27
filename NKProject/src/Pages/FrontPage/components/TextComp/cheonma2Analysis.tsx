19ead8c1c2158f64d2477bf0b362a8c8546ed015
AlternateFire <yseunghan34@gmail.com>
2025-06-27 00:23:50 -0400

    updated picture + text

--- NKProject/src/Pages/FrontPage/components/TextComp/cheonma2Analysis.tsx
+++ NKProject/src/Pages/FrontPage/components/TextComp/cheonma2Analysis.tsx
@@ -5,7 +5,7 @@ const textHeader = "Ch’ŏnma-2, M2020";
 const subTextHeader = `On the 10th of October 2020 marked the 75th Anniversary of the foundation of the Workers’ Party of Korea (WPK), the far-left party of the totalitarian one-party Democratic People’s Republic of Korea (DPRK). 
 This took place in P’yŏngyang, the North Korean capital, through Kim Il-sung Square. During this parade, a
 new Main Battle Tank (MBT) had appeared. The tank was completely different from North Korea's tradtional tank arsenal. So much in fact that has intrigued many military analysts and enthusiasts was seen for the first time and has aroused great interest.`;
-const text_1 = 'Not much is known about this tank, even its name remains a point of contention. The namesake \"Ch’ŏnma-2\" was first observed through unofficial means by analyzing KCTV publications of Kim Jong Un inspecting this tank sometime around May 2024.'
+const text_1 = 'Not much is known about this tank, even its name remains a point of contention. The namesake \"Ch’ŏnma-2\" (천마-2호) owas first observed through unofficial means by analyzing KCTV publications of Kim Jong Un inspecting this tank sometime around May 2024.'
 const text_2 = 'The Ch’ŏnma-2 remains a largely new design with very little in common with previous MBTs developed in North Korea.'
 
 export default function Cheonma2Analysis() {
@@ -69,11 +69,10 @@ export default function Cheonma2Analysis() {
                     sx={{
                         justifyContent: "center",
                         textAlign: "center",
-                        height: 330,
+                        height: 430,
                         width: 810,
-                 
                     }}
-                    src="src/Pages/FrontPage/components/TextComp/ImagesComp/M2020v1.png"
+                    src="src/Pages/FrontPage/components/TextComp/ImagesComp/equipment_overview.jpeg"
                 />
                 
             </Box>
