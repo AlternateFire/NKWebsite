25948f7aa4ebe46fc0b8ccac46aa9f37e0d246b9
AlternateFire <yseunghan34@gmail.com>
2026-01-25 23:03:17 -0500

    docs implemented

--- NKProject/vite.config.ts
+++ NKProject/vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	base: '/NKWebsite/',
-	server: {
-		watch: {
-			usePolling: true,
-		},
-	},
+  plugins: [react()],
+  base: '/NKWebsite/',   // Your GitHub repo name
+  build: {
+    outDir: '../docs'    // This builds to NKWebsite/docs
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
 });
